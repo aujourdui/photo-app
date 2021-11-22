161768c4b0ba6d2b17d7ad0872f6d97fe8799c4e
kyosuke ito <kyosuke.ito@gmail.com>
2021-11-21 23:12:52 -0800

    add google authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
-import { db } from "./firebase";
+import { db, auth } from "./firebase";
 import Modal from "@material-ui/core/Modal";
 import { makeStyles } from "@material-ui/core/styles";
-import { Button } from "@material-ui/core";
+import { Button, Input } from "@material-ui/core";
 
 const getModalStyle = () => {
   const top = 50;
-  const left = 50;
 
   return {
     top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate{-${top}%, -${left}`,
+    margin: "auto",
   };
 };
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    position: "absolute",
+    position: "relative",
     width: 400,
     backgroundColor: theme.palette.background.paper,
     border: "2px solid #000",
@@ -30,9 +28,35 @@ const useStyles = makeStyles((theme) => ({
 
 const App = () => {
   const classes = useStyles();
-  const [modalStyle] = React.useState(getModalStyle);
+  const [modalStyle] = useState(getModalStyle);
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log(authUser);
+        setUser(authUser);
+
+        if (authUser.displayName) {
+        } else {
+          return authUser.updateProfile({
+            displayName: username,
+          });
+        }
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [user, username]);
 
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
@@ -45,17 +69,49 @@ const App = () => {
     });
   }, []);
 
+  const signUp = (event) => {
+    event.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="app">
       <Modal open={open} onClose={() => setOpen(false)}>
         <div style={modalStyle} className={classes.paper}>
-          <h2>I'm a modal</h2>
+          <center>
+            <h3>PhotoApp</h3>
+          </center>
+          <form className="app__signup">
+            <Input
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              type="text"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>
+              Sign Up
+            </Button>
+          </form>
         </div>
       </Modal>
+
       <div className="app__header">
         <h3>PhotoApp</h3>
       </div>
-
       <Button onClick={() => setOpen(true)}>Sign Up</Button>
 
       <h1>Hello PhotoApp</h1>
